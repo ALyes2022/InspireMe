1b42d8de997df7b115ff69951e0a33b2ac45b5e1
alyes <alyes.its@gmail.com>
2023-06-25 13:23:47 +0200

    Implementing-pin-logic-and-pins-feed

--- inspireme_frondend/src/containers/Home.jsx
+++ inspireme_frondend/src/containers/Home.jsx
@@ -10,12 +10,13 @@ import { client } from '../client';
 import logoInspireMe from '../assets/logoInspireMe.png';
 import Pins from './Pin';
 import { userQuery } from '../utils/data';
+import { fetchUser } from '../utils/fetchUser';
 
 const Home = () => {
   const [toggleSideBar, setToggleSideBar] = useState(false);
   const [user, setUser] = useState(null)
   const scrollRef = useRef(null)
-  const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear()
+  const userInfo = fetchUser();
   useEffect(() => {
     const query = userQuery(userInfo?.sub); //to fetch userInfo from Sanity
     //console.log(userInfo?.sub);
